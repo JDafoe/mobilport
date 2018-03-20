c64d3fa28a9d7c270ae5e3ca4fc1d43632551a6f
JDafoe <imedia@imd-im-31a-5.camp.confederationc.on.ca>
2018-03-20 14:45:59 -0400

    Got Comp/Image to Work
    Got Comp/Image to Work

--- src/app.js
+++ src/app.js
@@ -1,15 +1,41 @@
-const {Button, TextView, ui} = require('tabris');
+const {Composite, TextView, ui, Button, ImageView} = require('tabris');
 
-let button = new Button({
-  centerX: 0, top: 100,
-  text: 'Show message'
+// Create composites and append children to them
+
+const IMAGE_PATH = 'https://mrmccormack.github.io/imd-learning-tabris/images/';
+
+
+let composite1 = new Composite({
+  left: 0, top: 0, bottom: 0, right: '50%',
+  background: '#0b3dac'
 }).appendTo(ui.contentView);
 
-let textView = new TextView({
-  centerX: 0, top: [button, 50],
-  font: '24px'
+let casinoimage = new ImageView({
+  top: 'prev() 10',
+  centerX: 0,
+  image: IMAGE_PATH + 'casino.jpg'
+}).appendTo(composite1);
+
+new Button({
+  left: 10, top: 10,
+  text: 'Button'
+}).on('select', ({target}) => target.text = 'Pressed ' + (++count) + ' times')
+  .appendTo(ui.contentView);
+
+
+new TextView({
+  left: 0, right: 0, top: '50%',
+  alignment: 'center',
+  text: 'Composite 1'
+}).appendTo(composite1);
+
+let composite2 = new Composite({
+  left: '50%', top: 0, bottom: 0, right: 0,
+  background: '#eaeaea'
 }).appendTo(ui.contentView);
 
-button.on('select', () => {
-  textView.text = 'Justin Dafoe!';
-});
+new TextView({
+  left: 0, right: 0, top: '50%',
+  alignment: 'center',
+  text: 'Composite 2'
+}).appendTo(composite2);
